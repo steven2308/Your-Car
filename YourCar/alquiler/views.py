b06ce4de3226baae3d8539a1fa770db34a8141b9
Steven <steven2308@outlook.com>
2013-04-12 19:23:15 -0500

    Funcionalidad de registro basica agregada y vista basica de registro agregada

--- YourCar/alquiler/views.py
+++ YourCar/alquiler/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.contrib.auth import login, authenticate, logout
-from YourCar.alquiler.models import Vehiculo
+from YourCar.alquiler.models import Vehiculo,ClienteAlquiler
+from django.contrib.auth.models import User
 
 def loginControl(request):
 	try:
@@ -20,6 +21,11 @@ def loginControl(request):
 
 
 def inicioControl(request):
+	conectado=False
+	nombre=""
+	if request.user.is_authenticated():
+		conectado=True
+		nombre=request.user.username
 	return render_to_response ('inicio.html',locals(), context_instance = RequestContext(request))
 		
 
@@ -39,6 +45,45 @@ def vehiculosPrueba(request):
 	vehiculos = Vehiculo.objects.all()	
 	return render_to_response('vehiculos.html',locals())
 
+
+def registroControl(request):
+	if not request.user.is_authenticated():
+		if request.method == 'POST':
+			usuario = User.objects.create_user(username=request.POST["nombreUsuario"], email=request.POST["email"], password=request.POST["contrasena"])
+			usuario.first_name = request.POST['nombre']
+			usuario.last_name = request.POST['apellido']
+			usuario.save()
+			fechaNacimiento = request.POST['fechaNacimiento']
+			telFijo = request.POST['telFijo']
+			telCelular = request.POST['telCelular']
+			genero = request.POST['genero']
+			tipoPersona = request.POST['tipoPersona']
+			tipoDocumento  = request.POST['tipoDocumento']
+			numDocumento = request.POST['numDocumento']
+			paisResidencia = request.POST['paisResidencia']
+			ciudadResidencia = request.POST['ciudadResidencia']
+			dirResidencia = request.POST['dirResidencia']
+			nombrePersonaContacto = request.POST['nombrePersonaContacto']
+			telContacto = request.POST['telContacto']
+			direccionContacto = request.POST['direccionContacto']
+			cliente = ClienteAlquiler(user = usuario, fechaNacimiento = fechaNacimiento, telFijo = telFijo,
+				telCelular = telCelular, genero = genero, tipoPersona = tipoPersona, tipoDocumento = tipoDocumento,
+				numDocumento = numDocumento, paisResidencia = paisResidencia, ciudadResidencia = ciudadResidencia,
+				dirResidencia = dirResidencia, nombrePersonaContacto = nombrePersonaContacto, telContacto= telContacto,
+				direccionContacto=direccionContacto)
+			cliente.save()
+			return HttpResponseRedirect('/')
+		else:
+			return render_to_response('registro.html', locals(), context_instance = RequestContext(request))
+	else:
+		conectado = True
+		return render_to_response('registro.html', locals(), context_instance = RequestContext(request))
+
+
+# Controlador del Cierre de Sesion
+def logoutControl(request):
+    logout(request)
+    return HttpResponseRedirect('/')
 
 
 
--- YourCar/urls.py
+++ YourCar/urls.py
@@ -5,8 +5,10 @@ admin.autodiscover()
 urlpatterns = patterns('YourCar.alquiler.views',
 	url(r'^$', 'inicioControl', name='vistaPrincipal'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^login/$', 'loginControl', name='vistaLogin'),    
+    url(r'^registro/$', 'registroControl', name='vistaRegistro'),
+    url(r'^login/$', 'loginControl', name='vistaLogin'),
     url(r'^portal/$', 'portalControl', name='vistaPortal'),    
-    url(r'^vehiculos/$', 'vehiculosPrueba', name='vistaVehiculos'),
+    url(r'^vehiculos/$', 'vehiculosPrueba', name='vistaVehiculos'),    
+    url(r'^logout/$', 'logoutControl', name='vistalogout'),
 )
 	
